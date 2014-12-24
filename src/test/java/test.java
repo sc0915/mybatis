87d05a94640d65834b304d14f6358ce93a81575d
SC <sunchaoo915@163.com>
2014-12-24 20:13:27 +0800

    有事例

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,7 @@
+import com.shinowit.dao.mapper.SexInfoMapper;
+import com.shinowit.dao.mapper.StuInfoMapper;
 import com.shinowit.dao.mapper.TeaInfoMapper;
-import com.shinowit.model.TeaInfo;
-import com.shinowit.model.TeaInfoExample;
+import com.shinowit.model.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by SC on 2014/12/23.
@@ -19,11 +21,17 @@ public class test extends AbstractJUnit4SpringContextTests {
     @Resource
     private TeaInfoMapper tea_dao;
 
+    @Resource
+    private StuInfoMapper stu_dao;
+
+    @Resource
+    private SexInfoMapper sex_dao;
+
     @Test
     public void test1(){
         TeaInfo tea=new TeaInfo();
 
-        tea.setTeaId("003");
+        tea.setTeaId("004");
 
         tea.setTeaName("王老师");
 
@@ -31,4 +39,40 @@ public class test extends AbstractJUnit4SpringContextTests {
 
        // Assert.assertTrue(i==1);
     }
+
+    @Test
+    public void test2(){
+        TeaInfoExample ex=new TeaInfoExample();
+        TeaInfoExample.Criteria tc=ex.createCriteria();
+        tc.andTeaIdEqualTo("001");
+        List<TeaInfo> tea=tea_dao.selectByExample(ex);
+        for(TeaInfo t:tea){
+            System.out.println(t.getTeaName());
+        }
+    }
+
+    @Test
+    public void test3(){
+        StuInfo stu=stu_dao.selectByPrimaryKey("1");
+        System.out.println(stu.getSexInfo().getSexType());
+    }
+
+    @Test
+    public void test4(){
+        SexInfo sex=sex_dao.selectByPrimaryKey("1");
+        for (StuInfo stu:sex.getStuList()){
+            System.out.println(stu.getStuName());
+        }
+    }
+
+    @Test
+    public void test5(){
+        StuInfoExample se=new StuInfoExample();
+        se.createCriteria().andSexIdEqualTo("1");
+        List<StuInfo> stulist=stu_dao.selectStuWithSexTypeByPrimaryKey(se);
+        System.out.println(stulist.get(0).getSexInfo().getSexType());
+    }
+
+
+
 }
